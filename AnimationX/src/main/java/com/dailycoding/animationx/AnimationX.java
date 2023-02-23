0b60bc909872ff3f53a1479a6e8fb10cf47a97ce
Subhojit <subhojitdp@gmail.com>
2023-02-24 04:23:48 +0530

    v.1.0.0

---
+++ AnimationX/src/main/java/com/dailycoding/animationx/AnimationX.java
@@ -0,0 +1,46 @@
+package com.dailycoding.animationx;
+
+
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.view.View;
+
+/********************************************
+ *     Created by DailyCoding on 24-Feb-23.  *
+ ********************************************/
+
+public class AnimationX {
+
+    public static void changeColor(Context context, View view, int colorFrom, int colorTo) {
+
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(500); // milliseconds
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                view.setBackgroundColor((int) animator.getAnimatedValue());
+            }
+
+        });
+        colorAnimation.start();
+    }
+
+    public static void pulseAnimation(Context context, View view) {
+        ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                view,
+                PropertyValuesHolder.ofFloat("scaleX", 1.2f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.2f));
+        scaleDown.setDuration(310);
+
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+
+        scaleDown.start();
+    }
+
+
+}
