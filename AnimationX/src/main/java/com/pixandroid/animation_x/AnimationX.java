ac3d8f3527694da4eed819a6e49b832dc8b159af
Subhojit Halder <36542546+pixAndroid@users.noreply.github.com>
2023-07-16 02:23:03 +0530

    v.2.9

--- AnimationX/src/main/java/com/pixandroid/animation_x/AnimationX.java
+++ AnimationX/src/main/java/com/pixandroid/animation_x/AnimationX.java
@@ -1,6 +1,8 @@
 package com.pixandroid.animation_x;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
@@ -18,6 +20,7 @@ import android.view.animation.TranslateAnimation;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 /********************************************
@@ -38,7 +41,8 @@ public class AnimationX {
         view.startAnimation(AnimationUtils.loadAnimation(context, R.anim.click_scale));
     }
 
-    public static void color_change_effect(Context context, View view, int colorFrom, int colorTo, int duration) {
+    public static void view_color_change_effect(Context context, View view, int colorFrom,
+                                                int colorTo, int duration, AnimationXListener listener) {
 
         ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
         colorAnimation.setDuration(duration); // milliseconds
@@ -50,10 +54,31 @@ public class AnimationX {
             }
 
         });
+
+        colorAnimation.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(@NonNull Animator animation, boolean isReverse) {
+                super.onAnimationStart(animation, isReverse);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (listener != null)
+                    listener.onAnimationCompleted();
+            }
+        });
+
         colorAnimation.start();
     }
 
-    public static void text_color_change_effect(Context context, TextView textView, int colorFrom, int colorTo, int duration) {
+    public static void text_color_change_effect(Context context,
+                                                TextView textView,
+                                                int colorFrom,
+                                                int colorTo,
+                                                int duration,
+                                                AnimationXListener listener) {
 
         if (colorFrom <= 0) {
             colorFrom = textView.getCurrentTextColor();
@@ -69,10 +94,26 @@ public class AnimationX {
             }
 
         });
+
+        colorAnimation.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(@NonNull Animator animation, boolean isReverse) {
+                super.onAnimationStart(animation, isReverse);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (listener != null)
+                    listener.onAnimationCompleted();
+            }
+        });
+
         colorAnimation.start();
     }
 
-    public static void pulse_effect(Context context, View view) {
+    public static void pulse_effect(Context context, View view, AnimationXListener listener) {
         ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
                 view,
                 PropertyValuesHolder.ofFloat("scaleX", 1.2f),
@@ -81,6 +122,22 @@ public class AnimationX {
 
         scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
         scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+
+        scaleDown.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(@NonNull Animator animation, boolean isReverse) {
+                super.onAnimationStart(animation, isReverse);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (listener != null)
+                    listener.onAnimationCompleted();
+            }
+        });
+
 
         scaleDown.start();
     }
@@ -120,7 +177,7 @@ public class AnimationX {
     }
 
 
-    public static void alphaAnimation(View view, float from_alpha, float to_alpha,  int duration, AlphaListener listener) {
+    public static void alphaAnimation(View view, float from_alpha, float to_alpha,  int duration, AnimationXListener listener) {
         AlphaAnimation alphaAnimation = new AlphaAnimation(from_alpha, to_alpha);
         alphaAnimation.setDuration(duration);
         alphaAnimation.setRepeatCount(0);
@@ -134,7 +191,8 @@ public class AnimationX {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                listener.onAnimationCompleted();
+                if (listener != null)
+                    listener.onAnimationCompleted();
             }
 
             @Override
@@ -144,12 +202,12 @@ public class AnimationX {
         });
     }
 
-    public interface AlphaListener {
+    public interface AnimationXListener {
         void onAnimationCompleted();
     }
 
 
-    public static void fade_out(View view, int duration) {
+    public static void fade_out(View view, int duration, AnimationXListener listener) {
         AlphaAnimation alphaAnimation = new AlphaAnimation(1f, 0.0f);
         alphaAnimation.setDuration(duration);
         alphaAnimation.setRepeatCount(0);
@@ -164,6 +222,9 @@ public class AnimationX {
             @Override
             public void onAnimationEnd(Animation animation) {
                 view.setVisibility(View.GONE);
+                if (listener != null) {
+                    listener.onAnimationCompleted();
+                }
             }
 
             @Override
@@ -173,7 +234,7 @@ public class AnimationX {
         });
     }
 
-    public static void fade_in(View view, int duration) {
+    public static void fade_in(View view, int duration, AnimationXListener listener) {
         AlphaAnimation alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
         alphaAnimation.setDuration(duration);
         alphaAnimation.setRepeatCount(0);
@@ -188,6 +249,8 @@ public class AnimationX {
             @Override
             public void onAnimationEnd(Animation animation) {
                 view.setVisibility(View.VISIBLE);
+                if (listener != null)
+                    listener.onAnimationCompleted();
             }
 
             @Override
@@ -197,33 +260,114 @@ public class AnimationX {
         });
     }
 
-    public static void smoothProgressAnimation(ProgressBar pb, int progressTo, long anim_duration) {
+    public static void smoothProgressAnimation(ProgressBar pb, int progressTo, long anim_duration, AnimationXListener listener) {
         //TODO : HOW TO SET MAX VALUE IN YOUR PROGRESS BAR?
         //TODO : pb.setMax(yourMaxValue * 100);
         ObjectAnimator animation = ObjectAnimator.ofInt(pb, "progress", pb.getProgress(), progressTo * 100);
         animation.setDuration(anim_duration);
         animation.setAutoCancel( true);
         animation.setInterpolator( new DecelerateInterpolator());
+        animation.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(@NonNull Animator animation, boolean isReverse) {
+                super.onAnimationStart(animation, isReverse);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (listener != null)
+                    listener.onAnimationCompleted();
+            }
+        });
+
         animation.start();
     }
 
-    public static void bottomToTop(View view, int duration, int fromY, int toY) {
+    public static void vertical_move(View view, int duration, int fromY, int toY, AnimationXListener listener) {
         TranslateAnimation animate = new TranslateAnimation(0,0,fromY,toY);
         animate.setDuration(duration);
         animate.setFillAfter(true);
+
+        animate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (listener != null) {
+                    listener.onAnimationCompleted();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
         view.startAnimation(animate);
         view.setVisibility(View.VISIBLE);
     }
 
-    public static void translate(View view, int duration, int fromX, int toX, int fromY, int toY) {
+    public static void horizontal_move(View view, int duration, int fromX, int toX, AnimationXListener listener) {
+        TranslateAnimation animate = new TranslateAnimation(fromX,toX,0,0);
+        animate.setDuration(duration);
+        animate.setFillAfter(true);
+
+        animate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (listener != null) {
+                    listener.onAnimationCompleted();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        view.startAnimation(animate);
+        view.setVisibility(View.VISIBLE);
+    }
+
+    public static void translate(View view, int duration, int fromX, int toX, int fromY, int toY, AnimationXListener listener) {
         TranslateAnimation animate = new TranslateAnimation(fromX,toX,fromY,toY);
         animate.setDuration(duration);
         animate.setFillAfter(true);
+        animate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (listener != null) {
+                    listener.onAnimationCompleted();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
         view.startAnimation(animate);
         view.setVisibility(View.VISIBLE);
     }
 
-    public static void digitCounter(int start_number, int end_number, final TextView textview) {
+    public static void digitCounter(int start_number, int end_number, final TextView textview, AnimationXListener listener) {
         ValueAnimator valueAnimator = ValueAnimator.ofInt(start_number, end_number);
         valueAnimator.setDuration(1500);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -231,18 +375,38 @@ public class AnimationX {
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 textview.setText(valueAnimator.getAnimatedValue().toString());
             }
+
+
+
         });
+
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(@NonNull Animator animation, boolean isReverse) {
+                super.onAnimationStart(animation, isReverse);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (listener != null)
+                    listener.onAnimationCompleted();
+            }
+        });
+
+
         valueAnimator.start();
 
     }
 
-    public static void spin_and_move_vertical(View view) {
+    public static void spin_and_move_vertical(View view, int fromY, int toY, int duration, AnimationXListener listener) {
         AnimationSet animationSet = new AnimationSet(true);
 
         TranslateAnimation a = new TranslateAnimation(
                0, 0,
-               2000,0);
-        a.setDuration(1000);
+               fromY,toY);
+        a.setDuration(duration);
 
         RotateAnimation r = new RotateAnimation(360f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
 //        r.setStartOffset(1000);
@@ -251,17 +415,56 @@ public class AnimationX {
         animationSet.addAnimation(r);
         animationSet.addAnimation(a);
 
+        animationSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (listener != null) {
+                    listener.onAnimationCompleted();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
         view.startAnimation(animationSet);
     }
 
 
-    public static void spin_animation(View view, int duration, int repeat_count) {
+    public static void spin_animation(View view, int duration, int repeat_count, AnimationXListener listener) {
         AnimationSet animationSet = new AnimationSet(true);
         RotateAnimation rotateAnimation = new RotateAnimation(360f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         rotateAnimation.setDuration(duration);
         rotateAnimation.setRepeatMode(Animation.RESTART);
         rotateAnimation.setRepeatCount(repeat_count);
         animationSet.addAnimation(rotateAnimation);
+
+        animationSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (listener != null) {
+                    listener.onAnimationCompleted();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
         view.startAnimation(animationSet);
     }
 
@@ -269,5 +472,53 @@ public class AnimationX {
         Animation shake = AnimationUtils.loadAnimation(mContext, R.anim.shake);
         view.startAnimation(shake);
     }
+
+    public static void error_animation(Context mContext, View view){
+        Animation shake = AnimationUtils.loadAnimation(mContext, R.anim.shake);
+        view.startAnimation(shake);
+    }
+
+    public static void shakeLeft(View view, Context context) {
+        view.startAnimation(AnimationUtils.loadAnimation(context, R.anim.shake_left));
+    }
+
+
+    public static void splash_spin_and_move_vertical(View view, int duration, AnimationXListener listener) {
+        AnimationSet animationSet = new AnimationSet(true);
+
+        TranslateAnimation a = new TranslateAnimation(
+                0, 0,
+                2000,0);
+        a.setDuration(duration);
+
+        RotateAnimation r = new RotateAnimation(360f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+//        r.setStartOffset(1000);
+        r.setDuration(1400);
+
+        animationSet.addAnimation(r);
+        animationSet.addAnimation(a);
+
+        animationSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (listener != null) {
+                    listener.onAnimationCompleted();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        view.startAnimation(animationSet);
+    }
+
 
 }
